2196e6b3b212a97bdb45dd7098f41b6c229d2738
Anisul Haque <92867085+Anisulh@users.noreply.github.com>
2022-05-20 16:44:51 -0400

    able to edit and save changes

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,18 +1,68 @@
+import React, { useState } from "react";
 
 
 
 export default function TodoItem (props) {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState('');
+  function handleChange(e) {
+    setNewName(e.target.value);  
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.edit(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
   
-  return (
-    <div className="todo-item">
-      <li> 
-        <h4 className="item-name">{props.name}</h4>
-      </li>
-      <p className="todo-created-date">{props.date}</p>
-      <input id="todo-0" type="checkbox" defaultChecked={props.complete} />
-      <button type="button" className="item-edit">Edit</button>
-      <button type="button" className="item-delete">Delete</button>
-      
+  const editingTemplate = (
+    <form  onSubmit={handleSubmit}>
+      <div>
+        <label>
+          New name for {props.name}
+        </label>
+        <input id={props.id}  type="text" value={newName} onChange={handleChange}/>
+      </div>
+      <div >
+        <button type="button" className="btn" onClick={() => setEditing(false)}>
+          Cancel
+          <span >renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn">
+          Save
+          <span>new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+  const viewTemplate = (
+    <div>
+      <div >
+          <input
+            id={props.id}
+            type="checkbox"
+            defaultChecked={props.completed}
+            onChange={() => props.toggleTaskCompleted(props.id)}
+          />
+          <label>
+            {props.name}
+          </label>
+        </div>
+        <div>
+          <button type="button" className="btn" onClick={() => setEditing(true)}>
+            Edit <span >{props.name}</span>
+          </button>
+          <button
+            type="button"
+            className="btn btn__danger"
+            onClick={() => props.deleteTask(props.id)}
+          >
+            Delete <span >{props.name}</span>
+          </button>
+        </div>
     </div>
-  )
+  );
+  
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
+
 }
--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -3,10 +3,22 @@ import Form from "../components/form"
 import Header from "../components/Header"
 import TodoItem from "../components/TaskItem"
 import { nanoid } from "nanoid"
+const DATA = [] //data stores the tasks in an array
 
 function List() {
-  const DATA = []
-  const [tasks, setTasks] = useState(DATA)
+  
+ const [tasks, setTasks] = useState(DATA)
+  //match the id of task that has its complete property changed to the array and invert the complete property
+ function toggleComplete(id){
+     const updatedTasks = tasks.map(task => {
+       if (id === task.id){
+         return {...task, complete: !task.complete}
+       } 
+       return task
+   })
+     setTasks(updatedTasks)
+  }
+  //for each task in the array, create a component in this page
   const taskList = tasks.map(task => (
     <TodoItem 
       name= {task.name}
@@ -14,13 +26,34 @@ function List() {
       complete= {task.complete}
       id= {task.id}
       key= {task.id}
+      toggleComplete= {toggleComplete}
+      delete= {deleteTask}
+      edit= {editTask}
     />
   ))
-
+//adding a task
+//name and date come from form.js
   function addTask(name, date){
     const newTask = {id:'task-'+nanoid(), name: name, date: date, complete: false}
     setTasks([...tasks, newTask])
   }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  //editing a task
+  function editTask(id, newName){
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id){
+        return {...task, name: newName}
+      } return task
+    })
+    setTasks(editedTaskList)
+  }
+
+
+  //tells how many tasks are remaining
   const taskNoun = taskList.length > 1 ? 'tasks' : 'task';
   const taskLeft = `You have ${taskList.length} ${taskNoun} remaining`;
 
