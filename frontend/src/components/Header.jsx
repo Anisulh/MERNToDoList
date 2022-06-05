0bcb28b49ed4a8f0a0f7a247725177a2a95e5627
Anisul Haque <92867085+Anisulh@users.noreply.github.com>
2022-06-05 12:46:06 -0400

    user is able to logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom";
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {logout, reset} from '../features/auth/authSlice'
 //About needs to be updated
 
-
 export default function Header () {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const {user} = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
     <header>
-      <h1 className="heading">ToDo List</h1>
+      <Link to='/' className="heading">ToDo List</Link>
       <nav>
         <ul>
           <li>
@@ -18,6 +31,18 @@ export default function Header () {
           <li>
             <Link to='/'> About</Link> 
           </li>
+          {user? <button className="btn" onClick={onLogout}>Logout</button> : (
+          <>
+            <li>
+              <Link to='/login' > Login </Link> 
+            </li>
+            <li>
+              <Link to='/register'> Register</Link> 
+             </li>
+          </>
+            
+          )}
+          
         </ul>
       </nav>
     </header>
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -27,6 +27,11 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   console.log(user)
 })
 
+//logout user
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
+
 
 export const authSlice= createSlice({
   name: 'auth',
@@ -54,6 +59,9 @@ export const authSlice= createSlice({
         state.user = null
         state.isError= true
         state.message = action.payload
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
       })
   }
 })
